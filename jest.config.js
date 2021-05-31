6149545ac7d1dbe2c271bf4cb8e40fbddc51104b
Josep Seto <jsetojseto@gmail.com>
2021-06-01 00:33:19 +0200

    jest config jsdom npm update

--- jest.config.js
+++ jest.config.js
@@ -26,6 +26,7 @@ module.exports = {
 	setupFilesAfterEnv: [
 		"<rootDir>/jest-addons/setup-tests.ts"
 	],
+	testEnvironment: 'jsdom',
 	moduleNameMapper: {
 		".+\\.(scss|css)$": "<rootDir>/jest-addons/__mocks__/file-mock.js",
 		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest-addons/__mocks__/file-mock.js",
